b716c0b101a21a54e11f5172b403b899a4d1143d
thakshilad <tdthakshi@gmail.com>
2016-01-13 09:17:20 +0530

    UI changes

--- app/src/main/java/com/google/android/gms/location/sample/geofencing/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/google/android/gms/location/sample/geofencing/GeofenceTransitionsIntentService.java
@@ -153,11 +153,11 @@ public class GeofenceTransitionsIntentService extends IntentService {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
         // Define the notification settings.
-        builder.setSmallIcon(R.drawable.ic_launcher)
+        builder.setSmallIcon(R.drawable.logo)
                 // In a real app, you may want to use a library like Volley
                 // to decode the Bitmap.
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(),
-                        R.drawable.ic_launcher))
+                        R.drawable.logo))
                 .setColor(Color.RED)
                 .setContentTitle(notificationDetails)
                 .setContentText(getString(R.string.geofence_transition_notification_text))
